f33ae7c3b5669eef56264af249d2684f74ad81bf
Raditya83 <130730553+Raditya83@users.noreply.github.com>
2024-10-07 14:09:45 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,8 +3,30 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+     let stack = [];
+    let map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
     
+    for (let i = 0; i < s.length; i++) {
+        if (map[s[i]]) {
+            // If it's an opening bracket, push it onto the stack
+            stack.push(s[i]);
+        } else {
+            // Otherwise, check if it matches the last opening bracket
+            let last = stack.pop();
+            if (map[last] !== s[i]) {
+                return false;
+            }
+        }
+    }
+    
+    // If stack is empty, all brackets were matched
+    return stack.length === 0;
 };
+
 
 module.exports = { isValid };
 
