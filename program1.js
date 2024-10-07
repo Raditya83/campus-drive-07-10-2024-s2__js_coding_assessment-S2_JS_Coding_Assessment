c323449529e74dd135dfc42d05270e3148d7116c
Raditya83 <130730553+Raditya83@users.noreply.github.com>
2024-10-07 14:16:24 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    // Stack to keep track of open brackets
+    let stack = [];
     
+    // Hash map to match opening and closing brackets
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    
+    // Iterate through each character in the string
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            let last = stack.pop();
+       if (map[last] !== char) {
+                return false;
+            }
+        }
+    }
+    
+   return stack.length === 0;
 };
 
 module.exports = { isValid };
+
 
 
